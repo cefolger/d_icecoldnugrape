a19c9c90aff10e8d59586d0d8ba840b16f4e66c4
Chris Folger <cefolger@gmail.com>
2012-10-08 18:31:25 -0400

    add dgrid

--- js/app.js
+++ js/app.js
@@ -9,13 +9,32 @@ var tabSelected = function() {
             "dojo/domReady!"
         ], function( JsonRest, arrayUtil, DataGrid ) {
             var linkStore = new JsonRest({target:"json/links.json"});
-            
+
+            alert('working on it');
             require([
+                "dgrid/Grid",
                 "dojo/store/Memory",
                 "dojo/data/ObjectStore",
                 "dojox/grid/DataGrid",
                 "dojo/domReady!"
-            ], function(Memory, ObjectStore, DataGrid){
+            ], function(Grid, Memory, ObjectStore, DataGrid){
+
+                alert(Grid);
+                var data = [
+                    { first: "Bob", last: "Barker", age: 89 },
+                    { first: "Vanna", last: "White", age: 55 },
+                    { first: "Pat", last: "Sajak", age: 65 }
+                ];
+
+                var grid = new Grid({
+                    columns: {
+                        first: "First Name",
+                        last: "Last Name",
+                        age: "Age"
+                    }
+                }, "firstdiv");
+                grid.renderArray(data);
+/*
                 new DataGrid({
                     store: ObjectStore({objectStore: linkStore}),
                     structure: [
@@ -24,7 +43,7 @@ var tabSelected = function() {
                     ],
                     autoHeight: true
                 }, "firstdiv").startup();
-                
+ */
                 isLinkGridSetup = true;
             });
         });
