8e1690bd65b93aa7ba215c40ef52abc188200e41
Chris Folger <cefolger@gmail.com>
2012-10-08 19:22:37 -0400

    add custom cell formatting for background color

--- js/app.js
+++ js/app.js
@@ -17,19 +17,35 @@ var tabSelected = function() {
                 "dojo/data/ObjectStore",
                 "dojox/grid/DataGrid",
                 "dojo/store/Cache",
+                "dojo/dom-attr",
                 "dojo/domReady!"
-            ], function(Grid, Memory, ObjectStore, DataGrid, Cache){
+            ], function(Grid, Memory, ObjectStore, DataGrid, Cache, domAttr){
 
                 var testStore = Observable(JsonRest({target:"json/links.json",idProperty: "label"}));
 
                 var grid = new Grid({
-                    columns: {
-                        label: "label",
-                        value: "value"
-                    },
+                    columns: [
+                        {
+                            label: "The Label",
+                            field: "label",
+                            formatter: function(value) {
+                                return '<b>' + value + '</b>';
+                            }
+                        },
+                        {
+                            label: "The value",
+                            field: "value",
+                            renderCell: function(object, value, node, options) {
+                                if(object.styling) {
+                                    domAttr.set(node, 'style', object.styling);
+                                }
+                            }
+                        }
+                    ],
                     store: testStore
                 }, "firstdiv");
 
+                grid.set('sort', 'label', true);
 /*
                 new DataGrid({
                     store: ObjectStore({objectStore: linkStore}),
