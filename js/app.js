440afeb512c9b3585b3d7bb47a9e729edb448c25
Chris Folger <cefolger@gmail.com>
2012-10-08 17:46:04 -0400

    only one tab

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var isLinkGridSetup = false;
-var isSongGridSetup = false;
 
-var linkTabSelected = function() {
+var tabSelected = function() {
     if( isLinkGridSetup === false ) {
         require([
             "dojo/store/JsonRest",
@@ -24,70 +23,12 @@ var linkTabSelected = function() {
                         {name:"value", field:"value"}
                     ],
                     autoHeight: true
-                }, "linkGridDiv").startup();
+                }, "firstdiv").startup();
                 
                 isLinkGridSetup = true;
             });
         });
     }
-};
-
-var songTabSelected = function() {
-    if( isSongGridSetup === false ) {
-        require([
-            "dojo/store/JsonRest",
-            "dojo/_base/array",
-            "dojox/grid/DataGrid",
-            "dojo/domReady!"
-        ], function( JsonRest, arrayUtil, DataGrid ) {
-            var songStore = new JsonRest({target:"json/songs.json"});
-            
-            require([
-                "dojo/store/Memory",
-                "dojo/data/ObjectStore",
-                "dojox/grid/DataGrid",
-                "dojo/domReady!"
-            ], function(Memory, ObjectStore, DataGrid){
-                new DataGrid({
-                    store: ObjectStore({objectStore: songStore}),
-                    structure: [
-                        {name:"label", field:"label", width: "50%"},
-                        {name:"value", field:"value"}
-                    ],
-                    autoHeight: true
-                }, "songGridDiv").startup();
-                
-                isSongGridSetup = true;
-            });
-        });
-    }
-};
-
-var handleTabEvents = function() {
-    require([
-        "dojo/ready",
-        "dojo/aspect",
-        "dijit/registry",
-        "dijit/layout/TabContainer",
-        "dijit/layout/ContentPane"
-    ], function( ready, aspect, registry ) {
-        ready(function() {
-            var tabContainer = registry.byId("tabcontainer");
-            var linkContentPanel = registry.byId("linkcontentpanel");
-            var songContentPanel = registry.byId("songcontentpanel");
-            
-            // http://jsfiddle.net/phusick/Mdh4w/
-            // link tab selected            
-            aspect.after(linkContentPanel, "_onShow", function() {
-                linkTabSelected();
-            });
-            
-            // song tab selected            
-            aspect.after(songContentPanel, "_onShow", function() {
-                songTabSelected();
-            });
-        });
-    });
 };
 
 // start here
@@ -102,7 +43,7 @@ var handleTabEvents = function() {
         ready(function() {
             parser.parse();
             
-            handleTabEvents();
+            tabSelected();
         });
     });
 })();
